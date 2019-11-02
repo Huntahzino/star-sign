60bf80145eabd60d4d0f08d630c1b7c0f45c6a71
Huntahzino <47115621+Huntahzino@users.noreply.github.com>
2019-11-02 13:54:22 +0000

    Create starsign.py
    I've been trying to make this piece of code work for a while now, but I've encountered numerous errors I can't fix. I use Visual Studio Code and clearly the code is written in python. 
    This is the message I get when I try to run the code:
    [Running] python -u "c:\Users\Candidate\Documents\Hello World\starsigns.py"
    'python' is not recognized as an internal or external command,
    operable program or batch file.
    
    [Done] exited with code=1 in 0.075 seconds

---
+++ starsign.py
@@ -0,0 +1,21 @@
+# this program will be able to tell you your star sign and it's key attributes
+
+dateOfBirth =input("What is your Date of Birth in format (DD/MM): ")
+dateOfBirth= int
+#method for each month
+def compareDoB(D1, D2):
+    if dateOfBirth >= D1 and dateOfBirth <= D2:
+        print
+
+#Capricorn
+compareDoB((20.01), (16.02))
+print("Your star sign is Capricorn. Known for their practical approach,n\
+     the Capricorn natives apply their intelligence and logic and take into account all the facts before making any decisions n\
+         Those who are born under the Zodiac Sign Capricorn have a strong feeling that their decisions and methods are perfect, n\
+             and will rarely change their views")
+
+#Aquarius
+compareDoB((12.02), (11.03))
+print("Your star sign is Aquarius. They have a potential to become great inventors, technical wizards, n\
+     scientists, researchers, analysts, etc. n\
+         They can be cold and unemotional at times and cannot relate to people who do not think like them.")
